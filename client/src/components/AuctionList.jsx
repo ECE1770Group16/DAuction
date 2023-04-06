024467417bdf65a941b5d102f0d62b4e389f52d7
JiayuanSun <mrsunjiayuan@gmail.com>
2023-04-06 13:51:32 -0400

    Combine bid and deposit transactions, add stages of auction

--- client/src/components/AuctionList.jsx
+++ client/src/components/AuctionList.jsx
@@ -1,69 +1,182 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
 import useEth from "../contexts/EthContext/useEth";
+import Web3 from "web3";
 
 function AuctionList() {
-	const { state: { contract, accounts } } = useEth();
-	const [itemArr, setItemArr] = useState([]);
-	const [price, setPrice] = useState(0);
-	const [id, setId] = useState(0)
-	const isInitialMount = useRef(true);
+  const {
+    state: { contract, accounts },
+  } = useEth();
+  const [itemArr, setItemArr] = useState([]);
+  const [price, setPrice] = useState(0);
+  const [id, setId] = useState(0);
+  const isInitialMount = useRef(true);
 
-	const getAll = async () => {
-		const result = await contract.methods.getAll().call({ from: accounts[0] });
-		setItemArr(result);
-		console.log(result);
-		console.log(accounts);
-	};
+  const getAll = async () => {
+    const result = await contract.methods.getAll().call({ from: accounts[0] });
+    setItemArr(result);
+    console.log(result);
+    // console.log(contract);
+    // console.log(window.ethereum.request);
+  };
 
-	const bid = async () => {
-		const newId = parseInt(id);
-		const newPrice = parseInt(price)
-		await contract.methods.bid(newPrice, newId).send({ from: accounts[0] });
-	};
+  const bid = async () => {
+    const newId = parseInt(id);
+    const newPrice = parseInt(price);
+    const deposit = (newPrice / 5).toString();
+    await contract.methods
+      .bid(newPrice, newId)
+      .send({
+        from: accounts[0],
+        value: Web3.utils.toHex(Web3.utils.toWei(deposit, "ether")),
+      })
+      .then((receipt) => {
+        console.log("Transaction receipt:", receipt);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    // await window.ethereum.request({
+    // 	method: 'eth_sendTransaction',
+    // 	params: [
+    // 		{
+    // 			from: accounts[0],
+    // 			to: "0xEa998fB4fa1F361f7aB6eF7506C495bF1C2BF380",
+    // 			value: "0xDE0B6B3A7640000"
+    // 		},
+    // 	],
+    // }).then((txHash) => console.log(txHash)).catch((error) => console.error(error));
+    // Web3.eth.sendTransaction({from: accounts[0],to: "0xEa998fB4fa1F361f7aB6eF7506C495bF1C2BF380", value: Web3.utils.toWei("1", "ether")})
+  };
 
-	useEffect(() => {
-		if (isInitialMount.current) {
-		   isInitialMount.current = false;
-		} else {
-			getAll();
-		}
-	},[accounts]);
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      getAll();
+      const web3 = new Web3(Web3.givenProvider || "ws://localhost:7545");
+      web3.eth.getBalance(contract._address).then((result) => {
+        // console.log(contract._address)
+        console.log(result);
+      });
+    }
+  }, [accounts]);
 
+  return (
+    <>
+      <div className="container">
+        <h3 className="p-3 text-center">Active Auctions</h3>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>content</th>
+              <th>bidder</th>
+              <th>price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemArr &&
+              itemArr
+                .filter((item) => item.status === "0")
+                .map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.content}</td>
+                    <td>{item.bidder}</td>
+                    <td>{item.price}</td>
+                  </tr>
+                ))}
+          </tbody>
+        </table>
 
-	return (
-		<>
-		<div className="container">
-            <h3 className="p-3 text-center">Current Auctions</h3>
-            <table className="table table-striped table-bordered">
-                <thead>
-                    <tr>
-						<th>ID</th>
-                        <th>Name</th>
-                        <th>content</th>
-						<th>bidder</th>
-                        <th>price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {itemArr && itemArr.map(item =>
-                        <tr key={item.id}>
-							<td>{item.id}</td>
-                            <td>{item.name}</td>
-                            <td>{item.content}</td>
-							<td>{item.bidder}</td>
-                            <td>{item.price}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-			<form onSubmit={bid}>
-				<input type="number" value={id} onChange={(e) => {setId(e.target.value)}} />
-				<input type="number" value={price} onChange={(e) => {setPrice(e.target.value)}}/>
-				<input type="submit"/>
-			</form>
-        </div>
-		</>
-  	)
+        <form onSubmit={bid}>
+          <div className="row">
+            <div className="col">
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Please input the id"
+                value={id}
+                onChange={(e) => {
+                  setId(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col">
+              <input
+                type="number"
+                className="form-control"
+                placeholder="Please input the price you want to bid"
+                value={price}
+                onChange={(e) => {
+                  setPrice(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col">
+              <input type="submit" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <hr />
+        <h3 className="p-3 text-center">Auctions wait for final payment</h3>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>content</th>
+              <th>bidder</th>
+              <th>price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemArr &&
+              itemArr
+                .filter((item) => item.status === "1")
+                .map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.content}</td>
+                    <td>{item.bidder}</td>
+                    <td>{item.price}</td>
+                  </tr>
+                ))}
+          </tbody>
+        </table>
+        <hr />
+        <h3 className="p-3 text-center">Closed Auctions</h3>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>content</th>
+              <th>bidder</th>
+              <th>price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {itemArr &&
+              itemArr
+                .filter((item) => item.status === "2")
+                .map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.content}</td>
+                    <td>{item.bidder}</td>
+                    <td>{item.price}</td>
+                  </tr>
+                ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default AuctionList;
--- truffle/contracts/Auction.sol
+++ truffle/contracts/Auction.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Auction {
     event Log(uint amount, uint gas);
+
     receive() external payable {
         emit Log(msg.value, gasleft());
     }
@@ -17,17 +18,32 @@ contract Auction {
         address bidder;
     }
 
-    constructor () {
-        addItem("item1", "This is the first item", 0);
-        addItem("item2", "This is the second item", 0);
-        addItem("item3", "This is the third item", 0);
+    constructor() {
+        addItem("item1", "This is the first item", 0, 0);
+        addItem("item2", "This is the second item", 0, 0);
+        addItem("item3", "This is the third item", 0, 0);
+        addItem("item4", "This is the item in status 1", 5, 1);
+        addItem("item5", "This is teh item in status 2", 10, 2);
     }
 
     uint public idCount;
     mapping(uint => Item) public itemMap;
 
-    function addItem(string memory name, string memory content, uint price) public returns (uint) {
-        itemMap[idCount] = Item(idCount, name, content, 1, price, 0x777A97E3dA0583ABEdd43e7C8f1Dc547d0129B63, 0x777A97E3dA0583ABEdd43e7C8f1Dc547d0129B63);
+    function addItem(
+        string memory name,
+        string memory content,
+        uint price,
+        uint status
+    ) public returns (uint) {
+        itemMap[idCount] = Item(
+            idCount,
+            name,
+            content,
+            status,
+            price,
+            0x777A97E3dA0583ABEdd43e7C8f1Dc547d0129B63,
+            0x777A97E3dA0583ABEdd43e7C8f1Dc547d0129B63
+        );
         ++idCount;
         return idCount;
     }
@@ -40,9 +56,9 @@ contract Auction {
         return itemArr;
     }
 
-    function bid(uint price, uint id) public returns (uint){
+    function bid(uint price, uint id) public payable returns (uint) {
         itemMap[id].price = price;
         itemMap[id].bidder = msg.sender;
-        return price;
+        return msg.value;
     }
 }
